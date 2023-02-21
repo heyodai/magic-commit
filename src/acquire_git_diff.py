f3cfaaa5a126a9a3910f4efdabae6267ac49a0a9
Odai Athamneh <heyodai@gmail.com>
2023-02-21 13:49:38 -0600

    rewrite script to use PyGithub
    This commit changes the approach we take to interacting with the GitHub
    API. Instead of trying to set the headers myself, we're now using a
    library to do so.

--- src/acquire_git_diff.py
+++ src/acquire_git_diff.py
@@ -1,139 +1,110 @@
-"""
-We found an excellent GitHub commit dataset on Kaggle. However, it doesn't list what each commit actually changed.
-
-Fortunately, it looks like this can be done via the GitHub API.
-
-@see https://github.com/heyodai/magic-commit/issues/1
-"""
+from github import Github
 import pandas as pd
-import requests
+import os
 import logging
 import time
 import pickle
 import sqlite3
-import os
-from rich import print
-from dotenv import load_dotenv
 
-GITHUB_API_TOKEN = os.environ['GITHUB_API_TOKEN']
+token = os.environ.get("GITHUB_API_TOKEN")
+g = Github(token)
 
-def main():
-    # Set up logging
-    epoch = int(time.time())
-    logging.basicConfig(
-        filename='logs/{}_acquire_git_diff.log'.format(epoch),
-        filemode='w',
-        level=logging.INFO,
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    )
-    log = logging.getLogger(__name__)
+# Set up logging
+epoch = int(time.time())
+logging.basicConfig(
+    filename='logs/{}_acquire_git_diff.log'.format(epoch),
+    filemode='w',
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+log = logging.getLogger(__name__)
 
-    # Load the current_row variable from the pickle file
-    try:
-        with open('data/current_row.pickle', 'rb') as f:
-            current_row = pickle.load(f)
-    except FileNotFoundError:
-        current_row = 0
+# Load the current_row variable from the pickle file
+try:
+    with open('data/current_row.pickle', 'rb') as f:
+        current_row = pickle.load(f)
+except FileNotFoundError:
+    current_row = 0
 
-    # Load the sqlite3 database connection
-    con = sqlite3.connect("data/git_diff.db")
-    cur = con.cursor()
+# Load the sqlite3 database connection
+con = sqlite3.connect("data/git_diff.db")
+cur = con.cursor()
 
-    # Create the raw table if it doesn't exist
-    cur.execute("""
-    CREATE TABLE IF NOT EXISTS raw (
-        rowid INTEGER PRIMARY KEY,
-        commit_hash TEXT,
-        author TEXT,
-        date TEXT,
-        message TEXT,
-        repo TEXT,
-        additions INTEGER, 
-        deletions INTEGER,
-        files_changed INTEGER,
-        patch TEXT
-    )
-    """)
+# Create the tables if they don't exist
+cur.execute("""
+CREATE TABLE IF NOT EXISTS commits (
+    rowid INTEGER PRIMARY KEY,
+    commit_hash TEXT,
+    author TEXT,
+    date TEXT,
+    message TEXT,
+    repo TEXT
+)
+""")
+cur.execute("""
+CREATE TABLE IF NOT EXISTS diff (
+    additions INTEGER, 
+    deletions INTEGER,
+    filename INTEGER,
+    patch TEXT
+)
+""")
 
-    # Load the dataset from Kaggle
-    df = pd.read_csv('data/full.csv')
-    length = len(df)
+# Load the dataset from Kaggle
+df = pd.read_csv('data/full.csv')
+length = len(df)
 
-    # Loop through each commit and get the diff for it
-    for index, row in df[current_row:].iterrows():
-        # Log the current row
-        log.info('Processing row {} of {}'.format(index, length))
-        print('Processing row {} of {}'.format(index, length))
+# Loop through each commit and get the diff for it
+for index, row in df[current_row:].iterrows():
+    # Log the current row
+    log.info('Processing row {} of {}'.format(index, length))
+    print('Processing row {} of {}'.format(index, length))
 
-        # Safety check to make sure we don't process the same row twice
-        # This should never happen, but just in case
-        if current_row > index:
-            log.info('Skipping row {} as it has already been processed'.format(index))
-            print('Skipping row {} as it has already been processed'.format(index))
-            continue
+    # Safety check to make sure we don't process the same row twice
+    # This should never happen, but just in case
+    if current_row > index:
+        log.info('Skipping row {} as it has already been processed'.format(index))
+        print('Skipping row {} as it has already been processed'.format(index))
+        continue
 
-        commit = row['commit']
-        repo = row['repo']
-        author = row['author']
-        date = row['date']
-        message = row['message']
+    commit = row['commit']
+    repo = row['repo']
+    author = row['author']
+    date = row['date']
+    message = row['message']
 
-        # Get the diff
-        try:
-            authorization = f'token {GITHUB_API_TOKEN}'
-            headers = {
-                "Accept": "application/vnd.github.v3+json",
-                "Authorization" : authorization,
-            }
-            r = requests.get('https://api.github.com/repos/{}/commits/{}'.format(repo, commit), headers=headers)
-            r.raise_for_status()
-        except requests.exceptions.HTTPError as err:
-            log.error('Error getting diff for commit {}: {}'.format(commit, err))
-            print('Error getting diff for commit {}: {}'.format(commit, err))
-            exit(1)
+    # Get the diff
+    repo = g.get_repo(repo)
+    commit = repo.get_commit(commit)
 
-        additions = 0
-        deletions = 0
-        files_changed = 0
-        patch = ''
-        for file in r.json()['files']:
-            additions = additions + file['additions']
-            deletions = deletions + file['deletions']
-            files_changed = files_changed + file['changes']
-            if 'patch' in file:
-                patch = patch + file['patch']
+    for file in commit.files:
+        additions = file.additions
+        deletions = file.deletions
+        filename = file.filename
+        patch = file.patch
 
-        # Insert the row into the database
-        cur.execute("INSERT OR REPLACE INTO raw VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (
-            index,
-            commit, 
-            author, 
-            date, 
-            message, 
-            repo, 
-            additions, 
-            deletions, 
-            files_changed, 
-            patch
-            )
-        )
+        # store this data in a new table in your database
+        cur.execute("""
+        INSERT INTO diff (additions, deletions, filename, patch)
+        VALUES (?, ?, ?, ?)
+        """, (additions, deletions, filename, patch))
 
-        # Save the current row to a pickle file
-        with open('data/current_row.pickle', 'wb') as f:
-            pickle.dump(index, f)
+    # Save the current row to a pickle file
+    with open('data/current_row.pickle', 'wb') as f:
+        pickle.dump(index, f)
 
-        # Commit the changes to the database
-        con.commit()
+    # Commit the changes to the database
+    con.commit()
 
-        # Sleep for 1 second to avoid rate limiting
-        time.sleep(1)
+    # Sleep for 1 second to avoid rate limiting
+    time.sleep(1)
 
-        # If we've reached the end of the dataset, write database to csv file
-        if index == length - 1:
-            log.info('Writing database to csv file')
-            print('Writing database to csv file')
-            df = pd.read_sql_query("SELECT * FROM raw", con)
-            df.to_csv('data/git_diff.csv', index=False)
+    # If we've reached the end of the dataset, write database to csv file
+    if index == length - 1:
+        log.info('Writing database to csv file')
+        print('Writing database to csv file')
+        df = pd.read_sql_query("SELECT * FROM raw", con)
+        df.to_csv('data/git_diff.csv', index=False)
 
-    # Close the database connection
-    con.close()
+# Close the database connection
+con.close()
