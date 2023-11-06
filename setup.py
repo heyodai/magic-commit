61e889b56cefa5b1d3a3b34818c161b4de9297ee
Odai Athamneh <heyodai@gmail.com>
2023-11-06 11:08:36 -0600

    Run `black` and `isort` on existing codebase

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
 setup(
-    name='magic-commit',
-    version='0.1.0',
-    packages=['magic_commit'],
+    name="magic-commit",
+    version="0.2.0",
+    packages=["magic_commit"],
     install_requires=[
-        'openai',
+        "openai",
     ],
     entry_points={
-        'console_scripts': [
-            'magic-commit = magic_commit.__main__:main',
+        "console_scripts": [
+            "magic-commit = magic_commit.__main__:main",
         ],
     },
 )
