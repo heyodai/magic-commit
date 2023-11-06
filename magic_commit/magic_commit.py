f4ee1b095d419852a5e300d3cbe0bca269c285e0
Odai Athamneh <heyodai@gmail.com>
2023-11-06 16:03:02 -0600

    Refactor commit message generation logic
    Refactored the `generate_commit_message` function to use the `run_git_diff` function to retrieve the git diff instead of relying on the `commit_messages` parameter. This simplifies the function as it no longer needs to process and split the commit messages. The `render_template` function was also added to handle rendering the commit message template. These changes improve code readability and maintainability.

--- magic_commit/magic_commit.py
+++ magic_commit/magic_commit.py
@@ -4,45 +4,68 @@ import subprocess
 import openai
 from jinja2 import Environment, PackageLoader
 
-from .config import get_api_key, set_api_key, get_model, set_model
+from .config import get_api_key, get_model, set_api_key, set_model
 
 
 class GitRepositoryError(Exception):
     """Custom exception for Git repository errors."""
+
     pass
 
 
 class OpenAIKeyError(Exception):
     """Custom exception for OpenAI API key errors."""
+
     pass
 
 
 def is_git_repository(directory: str) -> bool:
     """
     Check if a directory is a Git repository.
-    
+
     Parameters
     ----------
     directory : str
         The path to check.
-        
+
     Returns
     -------
     bool
         True if the directory is a Git repository, False otherwise.
     """
+    print(directory)
     return os.path.isdir(os.path.join(directory, ".git"))
+
+
+def run_git_diff(directory: str) -> str:
+    """
+    Run the `git diff --cached` command and return the result.
+
+    Parameters
+    ----------
+    directory : str
+        The directory to run the command in.
+
+    Returns
+    -------
+    str
+        The output of the command.
+    """
+    # TODO: Write unit tests for this function
+    return subprocess.run(
+        ["git", "diff", "--cached"], cwd=directory, capture_output=True, text=True
+    ).stdout
 
 
 def run_git_log(directory: str) -> subprocess.CompletedProcess:
     """
     Run 'git log' command in a specified directory.
-    
+
     Parameters
     ----------
     directory : str
         The directory to run the command in.
-        
+
     Returns
     -------
     subprocess.CompletedProcess
@@ -79,21 +102,21 @@ def get_commit_messages(directory: str) -> str:
         raise GitRepositoryError("The specified directory is not a Git repository.")
 
     result = run_git_log(directory)
-    
+
     if result.returncode != 0:
         raise GitRepositoryError("Unable to get commit messages.")
 
     return result.stdout
 
 
-def generate_commit_message(commit_messages: str, api_key: str, model: str) -> str:
+def generate_commit_message(diff: str, api_key: str, model: str) -> str:
     """
     Generate a commit message.
 
     Parameters
     ----------
-    commit_messages : str
-        The commit messages to use as context.
+    diff : str
+        The git diff to use.
     api_key : str
         The OpenAI API key.
     model : str
@@ -109,25 +132,50 @@ def generate_commit_message(commit_messages: str, api_key: str, model: str) -> s
     OpenAIKeyError
         If the OpenAI API key is not set.
     """
-    # Throw an error if the API key is not set
+    # Perform guard check for the OpenAI API key
     if not api_key:
         raise OpenAIKeyError("OpenAI API key not set.")
 
-    # Set the OpenAI API key
-    openai.api_key = api_key
-
-    # Render the template
-    commit_messages_list = commit_messages.strip().split("\n")
-    jinja = Environment(loader=PackageLoader("magic_commit", "templates")).get_template(
-        "chat_completion.jinja"
-    )
-    message = commit_messages_list[-1] if commit_messages_list else ""
-    rendered_template = jinja.render(message=message)
+    # Generate the prompt
+    system_msg = render_template("", "system")
+    user_msg = render_template(diff, "user")
 
     # Generate the commit message
-    messages = [{"role": "system", "content": rendered_template}]
+    messages = [
+        {"role": "system", "content": system_msg},
+        {"role": "user", "content": user_msg},
+    ]
+    openai.api_key = api_key
     response = openai.ChatCompletion.create(model=model, messages=messages)
     return response.choices[0].message.content.strip()
+
+
+def render_template(message: str, template_name: str) -> str:
+    """
+    Render the commit message template.
+
+    Parameters
+    ----------
+    message : str
+        The commit message to render.
+    template_name : str
+        The name of the template to use.
+
+    Returns
+    -------
+    str
+        The rendered commit message.
+    """
+    # TODO: Write unit tests for this function
+
+    # Render any escape sequences in the message
+    message.encode().decode("unicode_escape")
+
+    # Render and return the template
+    jinja = Environment(loader=PackageLoader("magic_commit", "templates")).get_template(
+        f"{template_name}.jinja"
+    )
+    return jinja.render(diff=message)
 
 
 def run_magic_commit(directory: str, api_key: str, model: str) -> str:
@@ -148,6 +196,6 @@ def run_magic_commit(directory: str, api_key: str, model: str) -> str:
     str
         The generated commit message.
     """
-    commit_messages = get_commit_messages(directory)
-    commit_message = generate_commit_message(commit_messages, api_key, model)
+    diff = run_git_diff(directory)
+    commit_message = generate_commit_message(diff, api_key, model)
     return commit_message
