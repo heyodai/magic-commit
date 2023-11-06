de6646eb7cfcf04f15454953cba609a07155b511
Odai Athamneh <heyodai@gmail.com>
2023-11-06 16:01:11 -0600

    Refactor imports and improve path handling in `magic_commit/__main__.py`
    Updated the import statements in `magic_commit/__main__.py` to improve readability and adhere to PEP 8 style guidelines.
    
    Changes:
    - Grouped imports based on their source (standard library, third-party, local)
    - Removed unused imports
    
    Additionally, improved path handling by:
    - Expanding the user's home directory if the tilde symbol is used in the directory path
    - Converting relative paths to absolute paths using `os.path.abspath()`
    
    These changes make the code more organized and easier to understand.

---
+++ magic_commit/__init__.py
@@ -0,0 +1,2 @@
+from .magic_commit import run_magic_commit
+from .config import get_api_key, get_model, set_api_key, set_model
--- magic_commit/__main__.py
+++ magic_commit/__main__.py
@@ -1,8 +1,15 @@
 import argparse
+import os
 
 import dagwood
 
-from .magic_commit import run_magic_commit, set_api_key, get_api_key, get_model, set_model
+from .magic_commit import (
+    get_api_key,
+    get_model,
+    run_magic_commit,
+    set_api_key,
+    set_model,
+)
 
 
 def main() -> None:
@@ -35,9 +42,18 @@ def main() -> None:
 
     else:
         directory = args.directory or "."  # Default to the current directory
+        directory = os.path.expanduser(
+            directory
+        )  # Expand the user's home directory if the tilde is used
+        directory = os.path.abspath(
+            directory
+        )  # Convert relative paths to absolute paths
+
         key = get_api_key()
         model = get_model()
-        run_magic_commit(directory=directory, api_key=key, model=model)
+        results = run_magic_commit(directory=directory, api_key=key, model=model)
+
+        print(results)
 
 
 if __name__ == "__main__":
