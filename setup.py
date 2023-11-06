5c577b686108fb08bd362c9af211bf7bae083014
Odai Athamneh <heyodai@gmail.com>
2023-11-06 16:26:22 -0600

    Update version to 0.3.0, include authors and email, add description and long description content type, and include URL
    - Updated the version in the setup.py file from 0.2.0 to 0.3.0 to reflect the current release.
    - Added the authors' names and email addresses to provide contact information.
    - Included a short description of the project, "Generate commit messages with OpenAI's GPT."
    - Specified the long description in the README.md file and set the content type to "text/markdown" for proper rendering on project platforms.
    - Provided the URL of the project repository, "https://github.com/heyodai/magic-commit", to give users easy access to the source code and project information.

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup, find_packages
 
+# Read in the README.md for the long description.
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
     name="magic-commit",
-    version="0.2.0",
+    version="0.3.0",
     packages=find_packages(),
     include_package_data=True,  # This line is needed to include non-code files
     package_data={
@@ -10,9 +14,14 @@ setup(
         "magic_commit": ["templates/*.html", "templates/*.jinja", "templates/*.jinja2"],
     },
     install_requires=["openai", "jinja2", "dagwood"],
+    author="Odai Athamneh, Rahul Mysore",
+    author_email="heyodai@gmail.com, rmf9t@umkc.edu",
     entry_points={
         "console_scripts": [
             "magic-commit = magic_commit.__main__:main",
         ],
     },
+    description="Generate commit messages with OpenAI\’s GPT.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/heyodai/magic-commit",
 )
