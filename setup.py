e1224c154aae2f4ab77f6f0d897a3ac572f37391
Odai Athamneh <heyodai@gmail.com>
2023-11-06 16:00:42 -0600

    Include additional templates and package files in the setup configuration
    Modified the `setup.py` file to include additional templates and package files in the setup configuration.
    
    Changes:
    - Changed the line `from setuptools import setup` to `from setuptools import setup, find_packages` to use the `find_packages()` function for automatic package discovery.
    - Added the `include_package_data=True` line to include non-code files in the package.
    - Added the `package_data` dictionary to specify the file patterns to include in each package.
    
    This change ensures that additional templates and package files are included when the package is installed.

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="magic-commit",
     version="0.2.0",
-    packages=["magic_commit"],
-    install_requires=[
-        "openai",
-    ],
+    packages=find_packages(),
+    include_package_data=True,  # This line is needed to include non-code files
+    package_data={
+        "": ["*.jinja", "*.jinja2", "*.html"],
+        "magic_commit": ["templates/*.html", "templates/*.jinja", "templates/*.jinja2"],
+    },
+    install_requires=["openai", "jinja2", "dagwood"],
     entry_points={
         "console_scripts": [
             "magic-commit = magic_commit.__main__:main",
